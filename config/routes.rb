39852bb463f7c0b9dccaba9f32db38ec7b94a8ab
MohammedBawazir <mohammed.k.bawzir@gmail.com>
2020-05-14 12:01:24 +0530

    second try to style

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'slides#home'
-  get 'heloo',to: 'slides#contact'
-  get 'slides/help'
+  get      'slides/about'
+  get      'slides/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 
